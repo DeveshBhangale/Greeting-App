9219f1077fead81c6efe13f2830d7a9ebc3cb560
MSDOMAIN1\dbhanga <bhangalecr7@gmail.com>
2021-09-25 00:20:21 +0530

    [Devesh]: added feature to display hello with name

--- greeting-app/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ greeting-app/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -3,19 +3,28 @@ package com.bridgelabz.greetingapp.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/GreetingController")
 public class GreetingController {
     
-    @RequestMapping("/jsonObject")
+    @GetMapping("/jsonObject")
     public Map<String, String> sayHello() {
         HashMap<String, String> map = new HashMap<>();
         map.put("key", "value");
-        map.put("foo", "bar");
+        map.put("foo", "foo");
         map.put("aa", "basdb");
         return map;
     }
+
+    @RequestMapping("/hello")
+	public String home(@RequestParam String name) {
+		return "Hello "+name;
+	}
 
 }
